cf84cf08c8a043ad80a2316b28132c6adb22d176
Joshua Cristol <joshcristol@gmail.com>
2020-08-02 21:36:58 -0500

    move game state into desk

--- components/desk.js
+++ components/desk.js
@@ -1,11 +1,57 @@
 import { createComponent } from 'cf-style-container';
+import { generateMineSweeperBoard } from '../utils/minesweeper';
+import Square from './square';
+import Mine from './mine';
+import Flag from './flag';
 
-const Desk = createComponent(({ boardSize }) => ({
+const PrimitiveDesk = createComponent(({ boardSize }) => ({
   width: 40 * boardSize + 2,
   height: 40 * boardSize + 2,
   border: `1px solid black`,
   display: 'flex',
   flexWrap: 'wrap'
 }));
+
+class Desk extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameBoard: generateMineSweeperBoard(props.boardSize)
+    };
+    this.flag = this.flag.bind(this);
+    this.reveal = this.reveal.bind(this);
+  }
+
+  reveal(event, { i }) {
+    this.state.gameBoard[i].isRevealed = true;
+    this.setState({ gameBoard: this.state.gameBoard });
+    event.preventDefault();
+  }
+
+  flag(event, { isRevealed, isFlagged, i }) {
+    if (!isRevealed) {
+      this.state.gameBoard[i].isFlagged = !isFlagged;
+      this.setState({ gameBoard: this.state.gameBoard });
+    }
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <PrimitiveDesk boardSize={this.props.boardSize}>
+        {this.state.gameBoard.map(cell => (
+          <Square
+            key={cell.i}
+            onClick={event => this.reveal(event, cell)}
+            onContextMenu={event => this.flag(event, cell)}
+          >
+            {cell.isRevealed && (cell.isMined ? <Mine /> : cell.distance)}
+            {cell.isFlagged && <Flag />}
+          </Square>
+        ))}
+      </PrimitiveDesk>
+    );
+  }
+}
 
 export default Desk;
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,7 @@ import Square from '../components/square';
 
 const Index = () => (
   <Layout title={`Minesweeper (active)`}>
-    <Desk boardSize={10}>
-      {generateMineSweeperBoard(10).map(cell => (
-        <Square
-          key={cell.i}
-          disabled={cell.i === 55 || cell.i === 10}
-          isMined={cell.isMined}
-          distance={cell.distance}
-        />
-      ))}
-    </Desk>
+    <Desk boardSize={10} />
   </Layout>
 );
 
