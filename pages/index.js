3b1eaa592eef6d6be8131cb999fe551d95185e65
Joshua Cristol <joshcristol@gmail.com>
2020-08-03 15:16:56 -0500

    hoist hame state into index page

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,75 @@ import Desk from '../components/desk';
 import Square from '../components/square';
 import Mine from '../components/mine';
 import Flag from '../components/flag';
+import Controls from '../components/controls';
+import {
+  createMineSweeperState,
+  areAllMinesFlagged
+} from '../utils/minesweeper';
 
-const Index = () => (
-  <Layout title={`Minesweeper (active)`}>
-    <Desk boardSize={10}>
-      {[...Array(100).keys()].map(i => (
-        <Square key={i} disabled={i === 55 || i === 10}>
-          {i === 10 && <Mine />}
-          {i === 25 && <Flag />}
-          {i === 77 ? '4' : ''}
-        </Square>
-      ))}
-    </Desk>
-  </Layout>
-);
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    const boardSize = 5;
+    this.state = {
+      gameBoard: createMineSweeperState(boardSize),
+      boardSize
+    };
+  }
+
+  reveal(event, cell, withRecursion = true) {
+    // this if statement tries to mimic original minesweeper
+    // in original minesweeper mines searched in the open reveal more then one game tile
+    // in this version of the game open tiles that are reveal
+    // all neighbors and neighbors neighbors specified by a depth
+    // if (withRecursion && isOpenCell(cell, this.state.gameBoard)) {
+    //   const neighborHood = getFrontierCells(cell, this.state.gameBoard, 2);
+    //   neighborHood.forEach(n => this.reveal(event, n, false));
+    // }
+
+    // update the gameboard state for this cell
+    cell.isRevealed = true;
+    this.setState({ gameBoard: this.state.gameBoard });
+
+    // if the cell is mined then trigger a game loss
+    if (cell.isMined) {
+      this.setState({ ...this.state, gameover: true, status: 'loss' });
+    }
+  }
+
+  flag(event, cell) {
+    if (!cell.isRevealed) {
+      cell.isFlagged = !cell.isFlagged;
+      this.setState({ gameBoard: this.state.gameBoard });
+    }
+    event.preventDefault();
+  }
+
+  render() {
+    if (areAllMinesFlagged(this.state.gameBoard)) {
+      // render the winning information
+      return <p>Game Over.</p>;
+    }
+    return (
+      <Layout title={`Minesweeper (active)`}>
+        <Desk boardSize={this.state.boardSize}>
+          {this.state.gameBoard.map(row =>
+            row.map(cell => (
+              <Square
+                key={cell.key}
+                onClick={event => this.reveal(event, cell)}
+                onContextMenu={event => this.flag(event, cell)}
+              >
+                {cell.isRevealed && (cell.isMined ? <Mine /> : cell.distance)}
+                {cell.isFlagged && <Flag />}
+              </Square>
+            ))
+          )}
+        </Desk>
+        <Controls />
+      </Layout>
+    );
+  }
+}
 
 export default Index;
