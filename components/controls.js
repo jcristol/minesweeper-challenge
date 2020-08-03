318ad5abdfce8ce43c1b562247c274b262c4998f
Joshua Cristol <joshcristol@gmail.com>
2020-08-03 15:16:37 -0500

    create controls component to handle game settings

---
+++ components/controls.js
@@ -0,0 +1,90 @@
+import { createComponent } from 'cf-style-container';
+
+const StyledDiv = createComponent(() => ({}), 'div');
+const Select = createComponent(() => ({}), 'select', ['value', 'onChange']);
+const H3 = createComponent(
+  () => ({
+    margin: 0
+  }),
+  'h3'
+);
+const Input = createComponent(() => ({}), 'input', ['value', 'onChange']);
+const CheckBox = createComponent(() => ({}), 'input', [
+  'type',
+  'value',
+  'onChange'
+]);
+const Button = createComponent(() => ({}), 'button', ['onClick']);
+const ControlDiv = createComponent(
+  () => ({
+    height: '4rem'
+  }),
+  'div'
+);
+
+const validBoardSizeInput = inputSize => {
+  return !isNaN(parseInt(inputSize));
+};
+
+class Controls extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardSize: '',
+      difficulty: 'easy',
+      authenticMode: false
+    };
+  }
+
+  formSubmit() {
+    if (validBoardSizeInput(this.state.boardSize)) {
+      const { difficulty, authenticMode, boardSize } = this.state;
+      const formSubmission = {
+        boardSize: parseInt(boardSize),
+        difficulty,
+        authenticMode
+      };
+      console.log(`${JSON.stringify(formSubmission)}`);
+    }
+  }
+
+  render() {
+    return (
+      <StyledDiv>
+        <ControlDiv>
+          <H3>Board Size</H3>
+          <Input
+            value={this.state.boardSize}
+            onChange={event => this.setState({ boardSize: event.target.value })}
+          />
+        </ControlDiv>
+        <ControlDiv>
+          <H3>Difficulty</H3>
+          <Select
+            value={this.state.difficulty}
+            onChange={event =>
+              this.setState({ difficulty: event.target.value })
+            }
+          >
+            <option>easy</option>
+            <option>medium</option>
+            <option>hard</option>
+          </Select>
+        </ControlDiv>
+        <ControlDiv>
+          <H3>Authentic Mode</H3>
+          <CheckBox
+            type="checkbox"
+            value={this.state.authenticMode}
+            onChange={() =>
+              this.setState({ authenticMode: !this.state.authenticMode })
+            }
+          />
+        </ControlDiv>
+        <Button onClick={() => this.formSubmit()}>Update Game Settings</Button>
+      </StyledDiv>
+    );
+  }
+}
+
+export default Controls;
